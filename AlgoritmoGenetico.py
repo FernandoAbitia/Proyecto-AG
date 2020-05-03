1dfd6ef0ef9010a01968864a904e19aeae7bff57
Fernando <fernando_abitia@hotmail.com>
2020-05-03 11:48:08 -0600

    Validación cadenas binarias

--- AlgoritmoGenetico.py
+++ AlgoritmoGenetico.py
@@ -15,11 +15,27 @@ class AlgoritmoGenetico:
         self._poblacion=[]
 
     def PoblacionInicial(self):
-        genes = np.random.randint(low=0, high=2, size=(self._individuos,16))
-        self._poblacion=[Cromosoma(gen) for gen in genes]
+        #Definimos una lista que almacenará las inversiones (para validación)
+        inv=[]
+        #Ciclo inicial que itera tantas veces como individuos haya
+        for i in range(self._individuos):
+            #Definimos un ciclo que se repetirá hasta obtener una lista de genes cuyas cadenas no superen
+            #el valor 10 en decimal (por cadena individual)
+            while True:
+                #Se genera una nueva lista de genes
+                genes = np.random.randint(low=0, high=2, size=(16))
+                #Obtenemos la lista de inversiones en valor decimal
+                inv=self.getInversion(genes)
+                #Si alguna de las cadenas supera el valor 10 en decimal, el ciclo while se reinicializa
+                if (any((c>10) for c in inv)):
+                    continue
+                #De lo contrario, agregamos a la población un nuevo cromosoma con una lista de genes con cadenas binarias válidas
+                self._poblacion.append(Cromosoma(genes))
+                #Salimos del ciclo while y se procede a la siguiente iteración del ciclo for
+                break
     
     def Algoritmo(self):
-       for generacion in self._generaciones:
+        for generacion in self._generaciones:
            self.Seleccion()
 
         ##Calcular aptitud a la población
@@ -27,21 +43,28 @@ class AlgoritmoGenetico:
         ##Cruza
         ##Mutación
         ##
+           
+    def getInversion(self, genes):
+        #Dividimos en 4 bits la cadena binaria del cromosoma
+        cadenas=np.array_split(genes,4)
+        #Definimos una lista que guardará las 4 inversiones (1 para cada lista/cadena)
+        x = []
+        #Abrimos otro ciclo para iterar sobre cada cadena del cromosoma (que representan las inversiones de cada zona)
+        for cadenaBinaria in cadenas:
+            c=0
+            #Iteramos sobre los bits de la cadena para obtener su equivalente en decimal
+            for bit in cadenaBinaria:
+                #Realizamos el desplazamiento y OR de los bits
+                c=(c<<1) | bit
+            #Agregamos el valor decimal de la cadena binaria a la lista
+            x.append(c)
+        return x
     
     def SetFitness(self):
+        inversiones=[]
         #Accedemos a un ciclo para iterar sobre los individuos de la población
         for cromosoma in self._poblacion:
-            #Dividimos en 4 bits la cadena binaria del cromosoma
-            cadenas=np.array_split(cromosoma.get_genes(),4)
-            inversiones=[]
-            #Abrimos otro ciclo para iterar sobre cada cadena del cromosoma (que representan las inversiones de cada zona) 
-            for cadenaBinaria in cadenas:
-                c=0
-                #Iteramos sobre los bits de la cadena para obtener su equivalente en decimal
-                for bit in cadenaBinaria:
-                    #Realizamos el desplazamiento y OR de los bits
-                    c=(c<<1) | bit
-                inversiones.append(c)
+            inversiones=self.getInversion(cromosoma.get_genes())
             #Aplicamos la función de aptitud y le asignamos el valor al cromosoma
             #Primero obtenemos los beneficios o ganancias por zona de acuerdo a la inversión
             beneficios=[self.df_beneficios[self.df_beneficios['Inversion']==inversiones[index]].iloc[0,(index+1)]for index in range(len(inversiones))]
@@ -54,8 +77,6 @@ class AlgoritmoGenetico:
             #Asignamos el valor de la función al individuo
             cromosoma.set_aptitud(fx)
             return
-            
-
         
     def Seleccion(self):
         torneo = []
@@ -87,4 +108,5 @@ class AlgoritmoGenetico:
 if __name__ == "__main__":
     A = AlgoritmoGenetico()
     A.PoblacionInicial()
-    A.SetFitness()
+    print(A)
+    #A.SetFitness()
